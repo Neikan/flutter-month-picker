3f22495bf23aa06d6910ba4a1d3aeeeb99a4f5cc
Neikan <sladkov.e.m@outlook.com>
2023-05-03 23:43:46 +0300

    feat(example): add main

---
+++ example/lib/main.dart
@@ -0,0 +1,7 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:customizable_month_picker_example/presentation/ui/components/cmp_app.dart';
+
+void main() => runApp(const CMPApp());
