4321524d7aa6fcb2a3f92d9d75c46f90e64e5d6b
Neikan <sladkov.e.m@outlook.com>
2023-05-10 15:06:49 +0300

    feat(project): add consts to presentation

---
+++ lib/src/presentation/consts/keys.dart
@@ -0,0 +1,2 @@
+const String keyCancel = 'keyCancel';
+const String keyConfirm = 'keyConfirm';
---
+++ lib/src/presentation/consts/localizations.dart
@@ -0,0 +1,7 @@
+// Project imports:
+import 'package:customizable_month_picker/src/presentation/consts/keys.dart';
+
+const defaultLabels = <String, String>{
+  keyCancel: 'CANCEL',
+  keyConfirm: 'OK',
+};
