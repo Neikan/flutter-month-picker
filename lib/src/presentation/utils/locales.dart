1fb4992d15fa22a8cc0b66f0daf154ad07f1bd91
Neikan <sladkov.e.m@outlook.com>
2023-05-10 15:08:08 +0300

    feat(project): add utils to presentation

---
+++ lib/src/presentation/utils/locales.dart
@@ -0,0 +1,15 @@
+// Flutter imports:
+import 'package:flutter/widgets.dart';
+
+String getLocale(
+  BuildContext context, {
+  Locale? selectedLocale,
+}) {
+  if (selectedLocale != null) {
+    return '${selectedLocale.languageCode}_${selectedLocale.countryCode}';
+  }
+
+  final Locale locale = Localizations.localeOf(context);
+
+  return '${locale.languageCode}_${locale.countryCode}';
+}
